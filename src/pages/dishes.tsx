70d7a0b0c1afa75768b7ad1d98641abdf39e11c3
Sumanth <60640082+Sumanth-Guduru@users.noreply.github.com>
2024-07-19 01:08:08 -0400

    errors fixed

--- src/pages/dishes.tsx
+++ src/pages/dishes.tsx
@@ -4,24 +4,25 @@ import Link from 'next/link';
 import { useEffect, useState } from 'react';
 import { Card, CardBody, CardFooter } from '@nextui-org/card';
 import Cook from '../app/images/cooking (1).png';
+
 type Recipe = {
   idMeal: string;
   strMeal: string;
   strInstructions: string;
+  strMealThumb: string;
 };
 
 export default function RecipePage() {
-  const [recipes, setRecipes] = useState([]);
-  const [allRecipes, setAllRecipes] = useState([]);
+  const [recipes, setRecipes] = useState<Recipe[]>([]);
+  const [allRecipes, setAllRecipes] = useState<Recipe[]>([]);
  
   useEffect(() => {
     const fetchRecipes = async () => {
-      // const response = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=');
       const response = await fetch('http://18.217.159.5:8000/api/recipes/');
       const data = await response.json();
       console.log(data);
       setRecipes(data.slice(0, 8));
-      setAllRecipes(data); // Store all recipes for filtering lat
+      setAllRecipes(data); // Adjust if necessary based on the actual data structure
     };
 
     fetchRecipes();
@@ -30,14 +31,17 @@ export default function RecipePage() {
   return (
     <main className="">
       <div className='text-center'>
-        <div className='flex justify-center inline-block space-y-12'><Image alt=''src= {Cook} width={100} height={100}/><h1 className='font-Burtons text-large sm:text-8xl '>Cookbook</h1></div>
+        <div className='flex justify-center inline-block space-y-12'>
+          <Image alt='' src={Cook} width={100} height={100}/>
+          <h1 className='font-Burtons text-large sm:text-8xl'>Cookbook</h1>
+        </div>
         <p className='font-cevaet'>Welcome!! Here are few Dishes... </p>
       </div>
       <div className="text-center">
         <h1 className="font-lemone text-8xl text-orange-400">Dishes</h1>
       </div>
       <div className="gap-2 grid grid-cols-2 sm:grid-cols-4 p-6">
-        {recipes.map((recipe, index) => (
+        {recipes.map((recipe) => (
           <Link key={recipe.idMeal} href={`/recipe_detail/${recipe.idMeal}`} passHref>
             <Card as="a" className="shadow-sm">
               <CardBody className="p-0 overflow-hidden">
@@ -46,7 +50,7 @@ export default function RecipePage() {
                   width={300}
                   height={200}
                   alt={recipe.strMeal}
-                  src={recipe.strMealThumb}
+                  src={recipe.strMealThumb || '/path/to/placeholder.png'} // Fallback image
                   className="object-cover"
                 />
               </CardBody>
@@ -57,9 +61,6 @@ export default function RecipePage() {
           </Link>
         ))}
       </div>
-      
-      
-      
     </main>
   );
 }
